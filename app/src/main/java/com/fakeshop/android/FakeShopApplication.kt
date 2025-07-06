ddde58872eeb9e13c74cee65ecec16e75a59c81a
Tendai  Bandawa <tonderaibandawa@gmail.com>
2025-07-06 16:25:37 +0200

    trigger hilt code gen

--- app/src/main/java/com/fakeshop/android/FakeShopApplication.kt
+++ app/src/main/java/com/fakeshop/android/FakeShopApplication.kt
@@ -1,8 +1,10 @@
 package com.fakeshop.android
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class FakeShopApplication: Application() {
 
     override fun onCreate() {
